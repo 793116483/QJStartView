c79569d5563bb92fb8c43b14bb44b35e685c4015
qujie <793116483@qq.com>
2017-04-20 17:48:14 +0800

    first commit

---
+++ QJStarView/QJStartView.h
@@ -0,0 +1,13 @@
+//
+//  QJStartView.h
+//  QJStarView
+//
+//  Created by 瞿杰 on 2017/4/20.
+//  Copyright © 2017年 yiniu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface QJStartView : NSObject
+
+@end
---
+++ QJStarView/QJStartView.m
@@ -0,0 +1,13 @@
+//
+//  QJStartView.m
+//  QJStarView
+//
+//  Created by 瞿杰 on 2017/4/20.
+//  Copyright © 2017年 yiniu. All rights reserved.
+//
+
+#import "QJStartView.h"
+
+@implementation QJStartView
+
+@end
